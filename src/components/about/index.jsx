67c852578f656511278df92258d1cbf03853d204
Alec Patterson <8888alec@gmail.com>
2025-02-24 16:52:05 -0500

    responsive about page

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -2,7 +2,7 @@ import clsx from "clsx";
 import React from "react";
 
 const ItemLayout = ({children, className}) => {
-    return <div className={clsx('custom-bg p-8 rounded-xl flex items-center justify-center space-y-8', className)} >
+    return <div className={clsx('custom-bg p-6 sm:p-8 rounded-xl flex items-center justify-center space-y-8', className)} >
         {children}
     </div>
 }
@@ -10,13 +10,13 @@ const ItemLayout = ({children, className}) => {
 const AboutDetails = () => {
     return (
         <section className='py-20 w-full'>
-            <div className="grid grid-cols-12 gap-8 w-full">
+            <div className="grid grid-cols-12 gap-4 xs:gap-6 md:gap-8 w-full">
 
-                <ItemLayout className={'col-span-8 row-span-2 flex-col items-start'}>
-                    <h2 className='text-2xl text-left w-full capitalize'>
+                <ItemLayout className={'col-span-full lg:col-span-8 row-span-2 flex-col items-start'}>
+                    <h2 className='text-xl md:text-2xl text-left w-full capitalize'>
                         Architect of Enchantment
                     </h2>
-                    <p className='font-light'>
+                    <p className='font-light text-xs sm:text-sm md:text-base'>
                     Recently graduated from the University of Florida with a bachelors in Computer Science, 3.93 GPA. 
                     I'm seeking a software engineering related role. Currently leading data migrations for Implementation Services at Enumerate. 
                     I have extensive experience in customer onboarding and related data migrations. 
@@ -24,14 +24,14 @@ const AboutDetails = () => {
                     </p>
                 </ItemLayout>
 
-                <ItemLayout className={'col-span-4 text-accent'}>
-                    <p className='font-semibold w-full text-left text-5xl'>
+                <ItemLayout className={'col-span-full xs:col-span-6 lg:col-span-4 text-accent'}>
+                    <p className='font-semibold w-full text-left text-2xl sm:text-5xl'>
                         3.95 <sub className="font-semibold text-base">GPA @ UF, cum laude</sub>
                     </p>
                 </ItemLayout>
 
-                <ItemLayout className={'col-span-4 text-accent'}>
-                    <p className='font-semibold w-full text-left text-5xl'>
+                <ItemLayout className={'col-span-full xs:col-span-6 lg:col-span-4 text-accent'}>
+                    <p className='font-semibold w-full text-left text-2xl sm:text-5xl'>
                         7 <sub className="font-semibold text-base">years of experience in Data Migration.</sub>
                     </p>
                 </ItemLayout>
@@ -47,15 +47,13 @@ const AboutDetails = () => {
                     />
                 </ItemLayout>
 
-                <ItemLayout className={'col-span-8 !p-0'}>
+                <ItemLayout className={'col-span-full md:col-span-8 !p-0'}>
                     <img className="w-full h-auto"
                         src="https://github-readme-stats.vercel.app/api?username=alecbytes&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false&show=prs_merged"
                         alt="AlecBytes"
                         loading="lazy" 
                     />
                 </ItemLayout>
-
-
 
                 <ItemLayout className={'col-span-full'}> 
                     <img className="w-full h-auto"
@@ -65,7 +63,7 @@ const AboutDetails = () => {
                     />
                 </ItemLayout>
 
-                <ItemLayout className={'col-span-6 !p-0'}> {/* should deploy my own github stat api */}
+                <ItemLayout className={'col-span-full md:col-span-6 !p-0'}> {/* should deploy my own github stat api */}
                     <img className="w-full h-auto"
                         src="http://github-readme-streak-stats.herokuapp.com?user=alecbytes&theme=dark&hide_border=true&background=FEFE5B00&ring=FEFE5B&currStreakLabel=FEFE5B"
                         alt="GitHub Streak"
@@ -73,7 +71,7 @@ const AboutDetails = () => {
                     />
                 </ItemLayout>
 
-                <ItemLayout className={'col-span-6 !p-0'}>
+                <ItemLayout className={'col-span-full md:col-span-6 !p-0'}>
                     <img className="w-full h-auto"
                         src="https://github-readme-stats.vercel.app/api/pin/?username=alecbytes&repo=PantryPal&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false&description_lines_count=2"
                         alt="AlecBytes"
