6d2555ac871477b18b5f24fcd5e324896b601775
Alec Patterson <8888alec@gmail.com>
2025-02-24 23:17:59 -0500

    analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import clsx from "clsx"
 import FireFliesBackground from "@/components/FireFliesBackground";
 import Sound from "@/components/Sound";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -22,6 +23,7 @@ export default function RootLayout({ children }) {
         <FireFliesBackground />
         <Sound />
         <div id="my-modal" />
+      <Analytics />
       </body>
     </html>
   );
