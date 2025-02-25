1d3cc93461bffe3bae52fb5d84deff2e0497f9cd
Alec Patterson <8888alec@gmail.com>
2025-02-25 09:30:37 -0500

    visual updates

--- src/app/(sub pages)/about/page.js
+++ src/app/(sub pages)/about/page.js
@@ -27,7 +27,7 @@ export default function Home() {
 
       <div className="relative w-full h-screen flex flex-col items-center justify-center">
         <div className="absolute flex flex-col items-center text-center top-1/2 sm:top-[60%] left-1/2 -translate-y-1/2 -translate-x-1/2">
-          <h1 className="font-bold text-6xl xs:text-7x1 sm:text-8xl lg:text-9xl text-accent">AlecBytes</h1>
+          <h1 className="font-bold text-6xl xs:text-7x1 sm:text-8xl lg:text-9xl text-accent">Alec Patterson</h1>
           <p className="font-light text-foreground text-ls mt-4">Meet the wizard behind this portfolio</p>
         </div>
       </div>
