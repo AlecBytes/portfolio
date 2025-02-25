783daefbf6dafafc05334129c703e9f0d9bc3ceb
Alec Patterson <8888alec@gmail.com>
2025-02-24 19:00:11 -0500

    stagger animate project page

--- src/components/projects/ProjectLayout.jsx
+++ src/components/projects/ProjectLayout.jsx
@@ -1,9 +1,17 @@
+import { motion } from 'framer-motion';
 import Link from 'next/link';
-import React from "react";
+
+const item= {
+    hidden: {opacity: 0, y:100},
+    show: {opacity: 1, y:0}
+}
+
+const ProjectLink = motion(Link)
 
 const ProjectLayout = ({name, description, date, demoLink}) => {
     return(
-        <Link
+        <ProjectLink
+        variants={item}
         href={demoLink}
         target={'_blank'}
          className='text-sm md:text-base flex items-center justify-between w-full relative rounded-lg overflow-hidden p-4 md:p-6 custom-bg'>
@@ -17,7 +25,7 @@ const ProjectLayout = ({name, description, date, demoLink}) => {
                 {new Date(date).toDateString()}
             </p>
         
-        </Link>
+        </ProjectLink>
     )
 }
 
