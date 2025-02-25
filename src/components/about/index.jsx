6c5fc8e20acea7a6db2ad220b786923bb4ec82fa
Alec Patterson <8888alec@gmail.com>
2025-02-24 23:05:16 -0500

    fixing ESlint rule violations

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -3,34 +3,34 @@ import ItemLayout from "./itemLayout";
 
 const AboutDetails = () => {
     return (
-        <section className='py-20 w-full'>
+        <section className="py-20 w-full">
             <div className="grid grid-cols-12 gap-4 xs:gap-6 md:gap-8 w-full">
 
-                <ItemLayout className={'col-span-full lg:col-span-8 row-span-2 flex-col items-start'}>
-                    <h2 className='text-xl md:text-2xl text-left w-full capitalize'>
+                <ItemLayout className={"col-span-full lg:col-span-8 row-span-2 flex-col items-start"}>
+                    <h2 className="text-xl md:text-2xl text-left w-full capitalize">
                         Architect of Enchantment
                     </h2>
-                    <p className='font-light text-xs sm:text-sm md:text-base'>
+                    <p className="font-light text-xs sm:text-sm md:text-base">
                     Recently graduated from the University of Florida with a bachelors in Computer Science, 3.93 GPA. 
-                    I'm seeking a software engineering related role. Currently leading data migrations for Implementation Services at Enumerate. 
+                    I&apos;m seeking a software engineering related role. Currently leading data migrations for Implementation Services at Enumerate. 
                     I have extensive experience in customer onboarding and related data migrations. 
-                    Reach out to me about anything cutting edge technology, I'd love to chat.
+                    Reach out to me about anything cutting edge technology, I&apos;d love to chat.
                     </p>
                 </ItemLayout>
 
-                <ItemLayout className={'col-span-full xs:col-span-6 lg:col-span-4 text-accent'}>
-                    <p className='font-semibold w-full text-left text-2xl sm:text-5xl'>
+                <ItemLayout className={"col-span-full xs:col-span-6 lg:col-span-4 text-accent"}>
+                    <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
                         3.95 <sub className="font-semibold text-base">GPA @ UF, cum laude</sub>
                     </p>
                 </ItemLayout>
 
-                <ItemLayout className={'col-span-full xs:col-span-6 lg:col-span-4 text-accent'}>
-                    <p className='font-semibold w-full text-left text-2xl sm:text-5xl'>
+                <ItemLayout className={"col-span-full xs:col-span-6 lg:col-span-4 text-accent"}>
+                    <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
                         7 <sub className="font-semibold text-base">years in customer onboarding data conversion</sub>
                     </p>
                 </ItemLayout>
 
-                <ItemLayout className={'col-span-full'}> 
+                <ItemLayout className={"col-span-full"}> 
                     <img className="w-full h-auto"
                         src="https://skillicons.dev/icons?i=git,github,java,js,ts,py,cpp,r,processing,html,css,sass,tailwind,regex,md,vue,react,nextjs,vite,nodejs,npm,cypress,postman,apollo,graphql,fastapi,supabase,postgres,mongodb,docker,aws,vercel,linux,ubuntu,windows,bash,powershell,vim,visualstudio,vscode,idea,cmake,pytorch,tensorflow,sklearn,anaconda,gmail,discord,raspberrypi,qt,latex
 "
@@ -50,7 +50,7 @@ const AboutDetails = () => {
                     />
                 </ItemLayout>
 
-                <ItemLayout className={'col-span-full md:col-span-8 !p-0'}>
+                <ItemLayout className={"col-span-full md:col-span-8 !p-0"}>
                     <img className="w-full h-auto"
                         src="https://github-readme-stats.vercel.app/api?username=alecbytes&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false&show=prs_merged"
                         alt="AlecBytes"
@@ -59,7 +59,7 @@ const AboutDetails = () => {
                 </ItemLayout>
 
 
-                <ItemLayout className={'col-span-full md:col-span-6 !p-0'}> {/* should deploy my own github stat api */}
+                <ItemLayout className={"col-span-full md:col-span-6 !p-0"}> {/* should deploy my own github stat api */}
                     <img className="w-full h-auto"
                         src="http://github-readme-streak-stats.herokuapp.com?user=alecbytes&theme=dark&hide_border=true&background=FEFE5B00&ring=FEFE5B&currStreakLabel=FEFE5B"
                         alt="GitHub Streak"
@@ -67,7 +67,7 @@ const AboutDetails = () => {
                     />
                 </ItemLayout>
 
-                <ItemLayout className={'col-span-full md:col-span-6 !p-0'}>
+                <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
                     <img className="w-full h-auto"
                         src="https://github-readme-stats.vercel.app/api/pin/?username=alecbytes&repo=PantryPal&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false&description_lines_count=2"
                         alt="AlecBytes"
@@ -75,7 +75,7 @@ const AboutDetails = () => {
                     />
                 </ItemLayout>
 
-                <ItemLayout className={'col-span-6 !p-0'}>
+                <ItemLayout className={"col-span-6 !p-0"}>
                     <img className="w-full h-auto"
                         src="https://github-readme-stats.vercel.app/api/pin/?username=alecbytes&repo=dbProject&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false&description_lines_count=2"
                         alt="AlecBytes"
@@ -83,7 +83,7 @@ const AboutDetails = () => {
                     />
                 </ItemLayout>
 
-                <ItemLayout className={'col-span-6 !p-0'}>
+                <ItemLayout className={"col-span-6 !p-0"}>
                     <img className="w-full h-auto"
                         src="https://github-readme-stats.vercel.app/api/pin/?username=alecbytes&repo=connexx-chainlink&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false&description_lines_count=2"
                         alt="AlecBytes"
