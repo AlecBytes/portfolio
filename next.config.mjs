4b309d0502e289e10221524d1273156da7f40935
Alec Patterson <8888alec@gmail.com>
2025-03-06 13:59:02 -0500

    setup chatbot route

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    // Any other config here (e.g. reactStrictMode)
+    reactStrictMode: true,
+  
+    async rewrites() {
+      return [
+        {
+          source: '/chatbot',
+          destination: `${process.env.CHATBOT_DOMAIN}/chatbot`,
+        },
+        {
+          source: '/chatbot/:path*',
+          destination: `${process.env.CHATBOT_DOMAIN}/chatbot/:path*`,
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
