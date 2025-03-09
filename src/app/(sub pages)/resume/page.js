86f4a7aadd3fe7f2b73cc2b939c80ecb8a183f73
Alec Patterson <8888alec@gmail.com>
2025-03-09 12:02:51 -0400

    udpate resume and added 2page resume

--- src/app/(sub pages)/resume/page.js
+++ src/app/(sub pages)/resume/page.js
@@ -34,14 +34,24 @@ export default function Resume() {
         <div className="absolute flex flex-col items-center text-center top-1/2 sm:top-[60%] left-1/2 -translate-y-1/2 -translate-x-1/2">
           <h1 className="font-bold text-6xl xs:text-7xl sm:text-8xl lg:text-9xl text-accent">Alec&apos;s Resume</h1>
           <p className="font-light text-foreground text-ls mt-4">Education, Skills, Experience, and Projects</p>
-          <Button 
-            href="/resume.pdf" 
-            target="_blank" 
-            rel="noopener noreferrer"
-            className="mt-4"
-          >
-            1-page PDF resume
-          </Button>
+          <div className="flex flex-col lg:flex-row lg:space-x-4 mt-4">
+            <Button 
+              href="/resume.pdf" 
+              target="_blank" 
+              rel="noopener noreferrer"
+              className="mt-4"
+            >
+              1-page PDF resume
+            </Button>
+            <Button 
+              href="/resume_extended.pdf" 
+              target="_blank" 
+              rel="noopener noreferrer"
+              className="mt-4"
+            >
+              2-page PDF resume
+            </Button>
+          </div>
         </div>
       </div>
 
